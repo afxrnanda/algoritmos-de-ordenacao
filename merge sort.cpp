d332fc49676fda433c77d9ede15a7c27f06d6c9b
Fernanda <101905797+afxrnanda@users.noreply.github.com>
2024-04-02 20:46:12 -0300

    Update merge sort.cpp

--- merge sort.cpp
+++ merge sort.cpp
@@ -63,7 +63,7 @@ void mergeSort(vector<int>& vetor, int l, int r) {
 }
 
 int main() {
-    vector<int> sizes = {10, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000, 1000000000};
+    vector<int> sizes = {10, 100, 1000, 10000, 100000, 1000000};
 
     for (int size : sizes) {
         vector<int> originalVector;
