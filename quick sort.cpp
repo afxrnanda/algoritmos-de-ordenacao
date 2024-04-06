8d4ca2e6930bc2f2715e7fa2b15238ad19f5ddb1
Fernanda <101905797+afxrnanda@users.noreply.github.com>
2024-04-06 19:50:06 -0300

    Update quick sort.cpp

--- quick sort.cpp
+++ quick sort.cpp
@@ -29,4 +29,26 @@ void quickSort(vector<int>& vetor, int low, int high) {
     if (low < high) {
         int pi = partition(vetor, low, high);
         quickSort(vetor, low, pi - 1);
-        qu
+        quickSort(vetor, pi + 1, high);
+    }
+}
+
+int main() {
+    vector<int> sizes = {10, 100, 1000, 10000, 30000, 50000, 70000, 100000, 300000, 500000, 700000, 1000000};
+
+    for (int size : sizes) {
+        vector<int> originalVector;
+        fillVector(originalVector, size);
+
+        vector<int> quickSorted = originalVector;
+
+        auto startQuick = high_resolution_clock::now();
+        quickSort(quickSorted, 0, size - 1);
+        auto endQuick = high_resolution_clock::now();
+        duration<double> durationQuick = endQuick - startQuick;
+
+        cout << "Tempo de execucao (Quick Sort) para " << size << " elementos: " << fixed << durationQuick.count() << " segundos" << endl;
+    }
+
+    return 0;
+}
